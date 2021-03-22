3ffe2ff6583375a8c82819d67649b9496d932f6e
BornToLose <twitchplayslore@gmail.com>
2021-03-22 17:24:34 +0100

    Moved common function to utils file

--- gacha/dex.js
+++ gacha/dex.js
@@ -1,4 +1,4 @@
-  
+
 function displayDex() {  
   
 	var Connect = new XMLHttpRequest();
@@ -82,38 +82,5 @@ function eraseDex() {
 	document.getElementById("trainers").innerHTML = "";
 	document.getElementById("mons").innerHTML = "";
 	displayDex();
-
-}
-
-function setSeason(runName) {  
-
-	var Connect = new XMLHttpRequest();
-	Connect.open("GET", "seasons.xml", false);
-	Connect.setRequestHeader("Content-Type", "text/xml");
-	Connect.send(null);
-
-	var response = Connect.responseXML;
-	var seasons = response.childNodes[0];
-	
-	for (i = 0; i < seasons.children.length; i++) {
-		
-		var currentSeason = seasons.children[i];
-		var color = seasons.children[i].getElementsByTagName("color")[0].textContent.toString();
-		//console.log(currentSeason);
-		//console.log(color);
-			
-		for (j = 0; j < currentSeason.getElementsByTagName("runs")[0].children.length; j++) {
-			
-			var currentRun = currentSeason.getElementsByTagName("runs")[0].children[j];
-			//console.log(currentRun);
-			
-			if (currentRun.textContent.toString() == runName) {
-				return "<i style=\"color:" + color + "\">" + runName + "</i>";
-			}
-			
-		}
-	}
-	
-	return "<i style=\"color:" + color + "\">" + runName + "</i>";
 
 }
--- gacha/pull.js
+++ gacha/pull.js
@@ -48,36 +48,3 @@ function pullCharacter() {
 	console.log(localStorage);
  
 } 
-
-function setSeason(runName) {  
-
-	var Connect = new XMLHttpRequest();
-	Connect.open("GET", "seasons.xml", false);
-	Connect.setRequestHeader("Content-Type", "text/xml");
-	Connect.send(null);
-
-	var response = Connect.responseXML;
-	var seasons = response.childNodes[0];
-	
-	for (i = 0; i < seasons.children.length; i++) {
-		
-		var currentSeason = seasons.children[i];
-		var color = seasons.children[i].getElementsByTagName("color")[0].textContent.toString();
-		//console.log(currentSeason);
-		//console.log(color);
-			
-		for (j = 0; j < currentSeason.getElementsByTagName("runs")[0].children.length; j++) {
-			
-			var currentRun = currentSeason.getElementsByTagName("runs")[0].children[j];
-			//console.log(currentRun);
-			
-			if (currentRun.textContent.toString() == runName) {
-				return "<i style=\"color:" + color + "\">" + runName + "</i>";
-			}
-			
-		}
-	}
-	
-	return "<i style=\"color:" + color + "\">" + runName + "</i>";
-
-}
