da1d9a7a1374d11d493ab339e62124be53809f57
BornToLose <twitchplayslore@gmail.com>
2021-03-24 17:11:16 +0100

    Completion rate on main page

--- gacha/utils.js
+++ gacha/utils.js
@@ -1,4 +1,35 @@
 
+function calcDexCompletion() {  
+
+	var Connect = new XMLHttpRequest();
+	Connect.open("GET", "pool.xml", false);
+	Connect.setRequestHeader("Content-Type", "text/xml");
+	Connect.send(null);
+
+	var response = Connect.responseXML;
+	var charas = response.childNodes[0];
+	
+	var totalCharas = charas.children.length;
+	var ownedCharas = 0.0;
+	
+	for (i = 0; i < charas.children.length; i++) {
+		
+		var currentChara = charas.children[i];
+					
+		var id = currentChara.getElementsByTagName("id");
+		
+		if (localStorage.getItem(id[0].textContent.toString()) == "owned") {
+			
+			ownedCharas += 1.0;
+			
+		}
+		
+	}
+	
+	return ((ownedCharas/totalCharas)*100).toFixed(1);
+
+}
+
 function setSeason(runName) {  
 
 	var Connect = new XMLHttpRequest();
