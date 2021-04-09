5612bc5d78054bdce8254819686916ece8de797c
BornToLose <twitchplayslore@gmail.com>
2021-04-09 08:05:47 +0200

    Minor text fix

--- gacha/codes.js
+++ gacha/codes.js
@@ -101,7 +101,7 @@ function randomFailMsg() {
 					"This code's use is forbidden in the Orre region.",
 					"<img src='misc/tppTeiHard.png'>",
 					"The miracle never happen.",
-					"Fine, I'll look after <a style='color:black' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>GHASTLY</a> for a while. Come see me in a while.",
+					"Fine, I'll look after <a style='color:black' href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>GASTLY</a> for a while. Come see me in a while.",
 					"<div style='color:red'>An exception occurred</div>Processor:&nbsp; ARM11 (core 0)<br>Exception type:&nbsp; data abort<br>Fault status:&nbsp; Translation - Section<br>Current process:&nbsp; kujira-2 (0004000000055E00)"];
 	var randomNum = Math.floor(Math.random() * msgArray.length);
 	
