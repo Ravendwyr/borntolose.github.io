a2dd4806a4f0a9c09561b23d0033dac545b8b5a5
BornToLose <twitchplayslore@gmail.com>
2022-12-13 23:55:39 +0100

    Added Meteoric Hoenn to HoFdex

--- hofdex/dex.js
+++ hofdex/dex.js
@@ -63,7 +63,7 @@ function getEmblem(emblem){
 function clearAll(dexType){
 	
 	if (dexType == "fakemon") {
-		regions = ["none", "kanto", "johto", "hoenn", "sinnoh", "sinnoh", "unova", "kalos", "alola", "galar", "naljo", "kohto"];
+		regions = ["none", "kanto", "johto", "hoenn", "sinnoh", "sinnoh", "unova", "kalos", "alola", "galar", "naljo", "kohto", "as-hoenn"];
 	} else {
 		regions = ["kanto", "johto", "hoenn", "sinnoh"/*, "unova", "kalos", "alola", "galar", "hisui", "paldea"*/];
 	}
