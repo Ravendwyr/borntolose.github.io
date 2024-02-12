b3a8ffab441d5ea8691530d5e4a912573bf5cf08
BornToLose <twitchplayslore@gmail.com>
2024-02-12 14:43:50 +0100

    Added timestamp script

---
+++ liveupdater/tpptime_script.js
@@ -0,0 +1,20 @@
+// the text area that the buttons will insert text into
+textarea = document.querySelector(".usertext-edit textarea")
+
+// create button bar
+const btnbox = document.createElement("div");
+btnbox.addClass('btnbox')
+
+// create buttons, and insert each button into the button bar
+$(`<a class="quickinserts">Time</a> `).appendTo(btnbox).click(() => {
+  textarea.value += timeElapsed()
+});
+$(`<a class="quickinserts">Poké</a> `).appendTo(btnbox).click(() => {
+  textarea.value += "Poké"
+});
+$(`<a class="quickinserts">₽</a> `).appendTo(btnbox).click(() => {
+  textarea.value += "₽"
+});
+
+// insert button bar into page
+textarea.parentElement.insertBefore(btnbox, textarea);
