47abb8db7bc253fdf71e81aa1c5803926b03e90c
BornToLose <twitchplayslore@gmail.com>
2024-02-12 16:40:15 +0100

    Updated timestamp script

--- liveupdater/tpptime_script.js
+++ liveupdater/tpptime_script.js
@@ -83,7 +83,8 @@ $(` <input type="button" value="ᴹɴ"> `).appendTo(btnbox).click(() => {
 $(` <input type="button" value="×"> `).appendTo(btnbox).click(() => {
   textarea.value += "×"
 });
-$(` <input type="button" value="Quick substitute"> `).appendTo(btnbox).click(() => {
+$(` <br/> `).appendTo(btnbox);
+$(` <a class="quickinserts">Quick substitute</a> `).appendTo(btnbox).click(() => {
   textarea.value = textarea.value.replace("$$", "₽").replace("Pokemon", "Pokémon").replace("Pokecenter", "Poké Center").replace("Pokemart", "Poké Mart").replace("pokecenter", "Poké Center").replace("pokemart", "Poké Mart").replace("pokeball", "Poké Ball").replace("Pokeball", "Poké Ball").replace("(male)", "♂").replace("(female)", "♀").replace("(pk)", "ᴾᴋ").replace("(mn)", "ᴹɴ").replace("(x)", "×").replace("NidoranM", "Nidoran♂").replace("NidoranF", "Nidoran♀");
 });
 $(` <br/> `).appendTo(btnbox);
