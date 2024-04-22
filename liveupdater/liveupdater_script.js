89043679b10c9da951807b5c3758306d4d508a54
BornToLose <twitchplayslore@gmail.com>
2024-04-22 22:55:47 +0200

    Updated live updater script

--- liveupdater/liveupdater_script.js
+++ liveupdater/liveupdater_script.js
@@ -1,7 +1,7 @@
 javascript: (
   function() { 
-    const gameName = "Super Gauntlet Colosseum";
-    const gameStartTime = 1713214800000;
+    const gameName = "Super Gauntlet XD";
+    const gameStartTime = 1713819600000;
 
 
     const timeElapsed = () => {   
